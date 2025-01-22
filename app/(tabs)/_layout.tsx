4ec8ec6a8452a9b4274ffc906ec3d030db7021ad
Samuel Klas <klassam95@gmail.com>
2025-01-22 22:43:38 +0200

    added basic tab layout

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,40 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Etusivu",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="recipes"
+        options={{
+          title: "Reseptit",
+          tabBarIcon: ({ color }) => (
+            //<FontAwesome size={28} name="home" color={color} />
+            <Ionicons size={28} name="document-text-sharp" color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="greens"
+        options={{
+          title: "Vihannekset",
+          tabBarIcon: ({ color }) => (
+            //<FontAwesome size={28} name="cog" color={color} />
+            <Ionicons size={28} name="checkmark" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
